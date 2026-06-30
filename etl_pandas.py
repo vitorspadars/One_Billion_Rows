dde04e002daa119372cf7c124ee7427ed61b2d4a
vitorspada <vitorspada.rs@gmail.com>
2026-06-30 20:42:14 -0300

    terminando o código com o pandas

--- etl_pandas.py
+++ etl_pandas.py
@@ -1,22 +1,49 @@
 import pandas as pd
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm  # importa o tqdm para barra de progresso
 
+CONCURRENCY = cpu_count()
 
-path_do_txt = 'data/measurements.txt'
+total_linhas = 100_000_000  # Total de linhas conhecido
+chunksize = 10_000_000  # Define o tamanho do chunk
+filename = "data/measurements.txt"  # Certifique-se de que este é o caminho correto para o arquivo
 
-df = pd.read_csv(
-                path_do_txt,
-                sep=';',
-                header=None,
-                names=['station', 'measure']
-                 
-                 )
+def process_chunk(chunk):
+    # Agrega os dados dentro do chunk usando Pandas
+    aggregated = chunk.groupby('station')['measure'].agg(['min', 'max', 'mean']).reset_index()
+    return aggregated
 
-df_agg = df.groupby('station')
+def create_df_with_pandas(filename, total_linhas, chunksize=chunksize):
+    total_chunks = total_linhas // chunksize + (1 if total_linhas % chunksize else 0)
+    results = []
 
-df_kpi = df_agg['measure'].agg({
+    with pd.read_csv(filename, sep=';', header=None, names=['station', 'measure'], chunksize=chunksize) as reader:
+        # Envolvendo o iterador com tqdm para visualizar o progresso
+        with Pool(CONCURRENCY) as pool:
+            for chunk in tqdm(reader, total=total_chunks, desc="Processando"):
+                # Processa cada chunk em paralelo
+                result = pool.apply_async(process_chunk, (chunk,))
+                results.append(result)
+
+            results = [result.get() for result in results]
+
+    final_df = pd.concat(results, ignore_index=True)
+
+    final_aggregated_df = final_df.groupby('station').agg({
         'min': 'min',
         'max': 'max',
         'mean': 'mean'
-        })
+    }).reset_index().sort_values('station')
 
-df_sorted = df_kpi.sort_values('station')
+    return final_aggregated_df
+
+if __name__ == "__main__":
+    import time
+
+    print("Iniciando o processamento do arquivo.")
+    start_time = time.time()
+    df = create_df_with_pandas(filename, total_linhas, chunksize)
+    took = time.time() - start_time
+
+    print(df.head())
+    print(f"Processing took: {took:.2f} sec")
